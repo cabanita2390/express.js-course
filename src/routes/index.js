4db0419c93fbde1bd6c25cd2fd1d88b180b342b2
Felipe Cabana <cabanita2390@hotmail.com>
2024-04-30 16:48:48 -0500

    Commit antes de incluir todo en carpeta API para deploy en vercel

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,17 +5,25 @@ const { Router } = require("express");
 const { userRouter } = require("./userRouter.js");
 const { postRouter } = require("./postRouter.js");
 
+const saludoIndex = (req, res) => {
+    try {
+        res.status(200).send('Hola desde el index')
+    } catch (error) {
+        res.status(500).json({
+            error: error
+        })
+    }
+}
 
 const router = Router();
+
+router.get('/', saludoIndex);
 
 router.use('/users', userRouter);
 router.use('/posts', postRouter);
 
 
 module.exports = router;
-
-
-
 
 
 
